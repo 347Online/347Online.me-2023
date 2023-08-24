ba39aeb7f7a25b8387a8d0586c4cb1c7e908ad97
347Online <38228372+347Online@users.noreply.github.com>
2023-08-23 21:10:11 -0500

    Basic Scramble generation on button click or spacebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import { randomScrambleForEvent } from "cubing/scramble";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [algorithm, setAlgorithm] = useState("Scrambling...");
+  const title = "Katie's Cube Scrambler";
+
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (e.code === "Space") {
+      e.preventDefault();
+      console.log("Space!");
+      scramble();
+    }
+  };
+
+  useEffect(() => {
+    document.title = title;
+
+    window.addEventListener("keyup", handleKeyPress);
+
+    return () => window.removeEventListener("keyup", handleKeyPress);
+  }, []);
+
+  const scramble = async () => {
+    const alg = await randomScrambleForEvent("333");
+    setAlgorithm(alg.toString());
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>{title}</h1>
+      <h2>{algorithm}</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button id="scrambleBtn" onClick={scramble}>
+          Scramble
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
