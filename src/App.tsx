88669b153d13644771eaf6ada679b7f6da51b269
347Online <38228372+347Online@users.noreply.github.com>
2023-08-23 22:36:43 -0500

    Move scramble code into own component
    This will be very useful when there are more than one cubing tool on the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,15 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect } from "react";
+import Scramble from "./components/Scramble/Scramble";
 
-import { randomScrambleForEvent } from "cubing/scramble";
-
-function App() {
-  const [algorithm, setAlgorithm] = useState("Scrambling...");
+const App = () => {
   const title = "Katie's Cube Scrambler";
-
-  const handleKeyPress = (e: KeyboardEvent) => {
-    if (e.code === "Space") {
-      e.preventDefault();
-      console.log("Space!");
-      scramble();
-    }
-  };
-
   useEffect(() => {
     document.title = title;
-    window.addEventListener("keyup", handleKeyPress);
-    scramble();
-
-    return () => window.removeEventListener("keyup", handleKeyPress);
   }, []);
-
-  const getFaceColor = (move: string): string => {
-  const faceColors = {
-    U: "white",
-    D: "yellow",
-    F: "green",
-    B: "blue",
-    L: "orange",
-    R: "red",
-  };
-    const matches = move.match(/[UDFBLR]{1}/);
-    if (!matches) return "white";
-    
-    const face = matches[0] as keyof typeof faceColors;
-
-    return faceColors[face];
-  };
-
-  const scramble = async () => {
-    const alg = await randomScrambleForEvent("333");
-    setAlgorithm(alg.toString());
-  };
-
-  const moves = algorithm.split(" ").map((move) => (
-    <span className="move" data-color={getFaceColor(move)}>
-      {` ${move} `}
-    </span>
-  ));
 
   return (
     <>
-      <h1 className="scrambleText">{moves}</h1>
-      <div className="card">
-        <button id="scrambleBtn" onClick={scramble}>
-          Scramble
-        </button>
-      </div>
+      <Scramble />
     </>
   );
 }
---
+++ src/components/Scramble/Scramble.tsx
@@ -0,0 +1,63 @@
+import "./Scramble.css"
+import { useEffect, useState } from "react";
+import { randomScrambleForEvent } from "cubing/scramble";
+
+const Scramble = () => {
+  const [algorithm, setAlgorithm] = useState("Scrambling...");
+
+  const scramble = async () => {
+    const alg = await randomScrambleForEvent("333");
+    setAlgorithm(alg.toString());
+  };
+
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (e.code === "Space") {
+      e.preventDefault();
+      console.log("Space!");
+      scramble();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyPress);
+    window.addEventListener("click", scramble);
+    scramble();
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyPress);
+      window.removeEventListener("click", scramble);
+    };
+  }, []);
+
+  const getFaceColor = (move: string): string => {
+    const faceColors = {
+      U: "white",
+      D: "yellow",
+      F: "green",
+      B: "blue",
+      L: "orange",
+      R: "red",
+    };
+    const matches = move.match(/[UDFBLR]{1}/);
+    if (!matches) return "white";
+
+    const face = matches[0] as keyof typeof faceColors;
+
+    return faceColors[face];
+  };
+
+  const moves = algorithm.split(" ").map((move) => (
+    <span className="move" data-color={getFaceColor(move)}>
+      {` ${move} `}
+    </span>
+  ));
+
+  return (
+    <>
+      <h1 className="scrambleText">{moves}</h1>
+      <h2>Tap, click, or press space to rescramble</h2>
+    </>
+  );
+};
+
+export default Scramble;
