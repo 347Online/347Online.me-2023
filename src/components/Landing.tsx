5a428786bd66c29cc4c8a9b8b551de709013a020
347Online <38228372+347Online@users.noreply.github.com>
2023-08-23 23:14:42 -0500

    Rebuild basic pages from current website

---
+++ src/components/Landing.tsx
@@ -0,0 +1,14 @@
+const Landing = () => {
+  return (
+    <>
+      <h1>Katie Janzen</h1>
+      <h2>Software Engineer | Game Developer</h2>
+      Hiya! I'm Katie, nice to meetcha!
+      <footer>
+        <a href="/social">Social</a>
+      </footer>
+    </>
+  );
+};
+
+export default Landing;
---
+++ src/components/Social.tsx
@@ -0,0 +1,23 @@
+const Social = () => (
+  <>
+    <h1>Social</h1>
+    <p>
+      GitHub: <a href="">347Online</a>
+    </p>
+    <p>
+      itch.io: <a href="">Konundream</a>
+    </p>
+    <p>
+      Mastodon: <a href="https://tech.lgbt/@347online">@347Online@tech.lgbt</a>,{" "}
+      <a href="https://mastodon.gamedev.place/@Konundream">
+        @Konundream@mastodon.gamedev.place
+      </a>
+    </p>
+
+    <footer>
+      <a href="/">Home</a>
+    </footer>
+  </>
+);
+
+export default Social;
