fdc6a16d113f1bf3784d3f15fc33938f06993a33
347Online <38228372+347Online@users.noreply.github.com>
2023-08-26 20:55:34 -0500

    Enable dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
-import { createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 const router = createBrowserRouter(routes);
 
 export const App = () => (
   <>
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <CssBaseline enableColorScheme>
+        <RouterProvider router={router} />
+      </CssBaseline>
     </ThemeProvider>
   </>
 );
