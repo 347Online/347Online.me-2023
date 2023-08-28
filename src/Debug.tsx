ee5910678ab703351fdc1e7b8281a7ac9042296f
347Online <38228372+347Online@users.noreply.github.com>
2023-08-27 23:02:03 -0500

    Cleanup in Debug component

--- src/Debug.tsx
+++ src/Debug.tsx
@@ -1,28 +1,13 @@
-type Expose = Record<string, unknown>;
-
-interface Target {
-  debug: Expose | undefined;
-}
-
 export interface DebugProps {
-  expose: Expose;
+  expose: Record<string, unknown>;
 }
 
 const DEV_MODE = import.meta.env.DEV;
-const global = window as unknown as Target;
-
-const assign = (expose: Expose, target: Target = global) => {
-  if (DEV_MODE) {
-    if (!target.debug) {
-      target.debug = {};
-    }
-
-    Object.assign(target.debug, expose);
-  }
-};
 
 export const Debug = ({ expose }: DebugProps) => {
-  assign(expose);
+  if (DEV_MODE) {
+    Object.assign(window, expose);
+  }
 
   return null;
 };
