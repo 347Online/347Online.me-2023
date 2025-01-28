cd4c8ef12e38c9306167f00ee11294482653a8b6
347Online | Katie Janzen <katiejanzen@347online.me>
2025-01-28 03:39:14 -0600

    Wrap build-resume.sh in Nix flake
    Include all defined scripts in devShell

--- flake.nix
+++ flake.nix
@@ -6,22 +6,35 @@
 
   outputs =
     {
-      self,
       nixpkgs,
       utils,
+      ...
     }:
     utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        scripts = {
+          build-resume = pkgs.writeShellApplication {
+            name = "build-resume";
+            runtimeInputs = with pkgs; [
+              nodePackages.prettier
+            ];
+            text = builtins.readFile ./build-resume.sh;
+          };
+        };
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            yarn
-            pandoc
-          ];
+          buildInputs =
+            with pkgs;
+            [
+              yarn
+              pandoc
+            ]
+            ++ builtins.map (x: x.value) (lib.attrsToList scripts);
         };
+        packages = scripts;
       }
     );
 }
