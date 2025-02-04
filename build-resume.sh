aafb6c234c90c5e8cd4a36594f8e4f89d03da16d
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-04 17:23:38 -0600

    Build all markdown files in resume/

--- build-resume.sh
+++ build-resume.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
-pandoc resume/resume.md -t plain -o public/resume.txt
-pandoc resume/resume.md -t html --template resume/template.html | prettier --stdin-filepath foo.html > public/resume.html
-pandoc resume/resume.md -t html --template resume/template.html --css public/resume.css -o public/resume.pdf
+shopt -s globstar
+
+for x in resume/*.md; do
+  fname="$(basename "$x")"
+  template_path="resume/${fname%.*}.template.html"
+  css_path="resume/${fname%.*}.css"
+
+  # Plaintext
+  pandoc "$x" -t plain -o "public/${fname%.*}.txt"
+
+  template () {
+    if [[ -f $template_path ]]; then
+      echo "--template" "$template_path"
+    else
+      echo "-s"
+    fi
+  }
+
+  css () {
+    if [[ -f $css_path ]]; then
+      echo " --css" "$css_path"
+    fi
+  }
+
+  # HTML
+  eval "pandoc $x -t html $(template)" | prettier --stdin-filepath foo.html > "public/${fname%.*}.html"
+
+  # PDF
+  eval "pandoc $x -t html $(template)$(css) -o public/${fname%.*}.pdf"
+done
