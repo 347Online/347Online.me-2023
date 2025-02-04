145882bf1fac7091763bb4bd65bd9985cbb6219b
347Online | Katie Janzen <katiejanzen@347online.me>
2025-02-04 17:46:05 -0600

    Embed shell script in flake
    Ensure validity checks

--- flake.nix
+++ flake.nix
@@ -21,7 +21,38 @@
               nodePackages.prettier
               wkhtmltopdf
             ];
-            text = builtins.readFile ./build-resume.sh;
+            text = ''
+              shopt -s globstar
+
+              for x in resume/*.md; do
+                fname="$(basename "$x")"
+                template_path="resume/''${fname%.*}.template.html"
+                css_path="resume/''${fname%.*}.css"
+
+                # Plaintext
+                pandoc "$x" -t plain -o "public/''${fname%.*}.txt"
+
+                template () {
+                  if [[ -f $template_path ]]; then
+                    echo "--template" "$template_path"
+                  else
+                    echo "--template" "resume/template.html"
+                  fi
+                }
+
+                css () {
+                  if [[ -f $css_path ]]; then
+                    echo " --css" "$css_path"
+                  fi
+                }
+
+                # HTML
+                eval "pandoc $x -t html $(template)" | prettier --stdin-filepath foo.html > "public/''${fname%.*}.html"
+
+                # PDF
+                eval "pandoc $x -t html $(template)$(css) -o public/''${fname%.*}.pdf"
+              done
+            '';
           };
         };
       in
